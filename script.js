017b7138f4ff5983f2d314a17528704e2d3eef55
Shihab <shihabdinajpur@gmail.com>
2020-02-16 13:52:29 +0600

    Initial Staging

---
+++ script.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function() {
+
+	
+	jQuery(".profile-photo, .name").click(function() {
+		jQuery(this).closest("div").find(".popup-outer").fadeIn();
+	});
+
+
+	jQuery(".ui-close").click(function() {
+		jQuery(this).parents("div").find(".popup-outer").fadeOut();
+	});
+
+
+});
